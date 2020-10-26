6c7fbcf1b05dcde7ee019850bdaefbe93d1cfb20
Ednan Jhony <ednanjhony@yahoo.com.br>
2020-10-26 07:38:05 -0300

    first commit

---
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -0,0 +1,15 @@
+import React, { useRef } from 'react';
+
+import { Container } from './styles';
+
+const ModelsWrapper: React.FC = ({ children }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  return (
+    <Container ref={wrapperRef}>
+      {children}
+    </Container>
+  );
+};
+
+export default ModelsWrapper;
